558f7deb93c4919d91b9fc97d82fbdc34ffa6103
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-24 15:58:51 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,24 @@
+class Board
+  attr_accessor :cells
+  
+  def initialize
+    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+  
+  def reset!
+    self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+  
+  def display
+    puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+    puts "------------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
+    puts "------------"
+    puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+  end
+  
+  def position
+    binding.pry
+  end
+  
+end
